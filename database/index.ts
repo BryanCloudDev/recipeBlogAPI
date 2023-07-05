ef7e3c2c20991f9d03643df8bddd3b0d3356daaa
Bryan Portillo <bportillo701@gmail.com>
2023-07-05 17:19:22 -0600

    impoerting models

--- database/index.ts
+++ database/index.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
+import * as models from '../models'
+
+const modelsArray = Object.values(models)
 
 export const AppDataSource = new DataSource({
   database: process.env.DATABASE_NAME,
-  entities: [],
+  entities: [...modelsArray],
   host: process.env.DATABASE_HOST,
   logging: false,
   migrations: [],
