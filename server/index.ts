19e47cee9c573bddf0bffcf4733ba903c9a3c36d
Bryan Portillo <bportillo701@gmail.com>
2023-07-06 15:55:00 -0600

    adding user router to server

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,20 @@
-import express, { Router } from 'express'
+import express from 'express'
 import { makeDBConnection } from '../database'
 import 'dotenv/config'
 import cors from 'cors'
+import RouterBase from '../routes/RouterBase'
+import UserRouter from '../routes/UserRouter'
 
-export default class Server {
+export default class Server extends RouterBase {
+  readonly userRouter: UserRouter
+
   constructor(
     readonly _app = express(),
-    readonly _apiRouter = Router(),
     readonly _port = Number(process.env.PORT),
     readonly _apiRoute = '/api'
   ) {
+    super()
+    this.userRouter = new UserRouter()
     void this.connectToDB()
     this.middleware()
     this.routes()
@@ -29,14 +34,14 @@ export default class Server {
 
   private routes(): void {
     // API route
-    this._app.use(this._apiRoute, this._apiRouter)
+    this.getRouter.use(this.userRouter._userRoute, this.userRouter.getRouter)
+
+    this._app.use(this._apiRoute, this.getRouter)
   }
 
   public listener(): void {
     this._app.listen(this._port, () => {
-      console.log(
-        `⚡️[server]: Server is running at http://localhost:${this._port}`
-      )
+      console.log(`⚡️[server]: Server is running at http://localhost:${this._port}`)
     })
   }
 }
