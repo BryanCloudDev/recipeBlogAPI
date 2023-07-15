21c07bea394e6fd639650ccf760d0ad67c7260d2
Bryan Portillo <bportillo701@gmail.com>
2023-07-15 17:56:36 -0600

    adding role prop to request interface

---
+++ dto/role/index.ts
@@ -0,0 +1,3 @@
+export * from './IRoleMiddleWare'
+export * from './IRoleRepository'
+export * from './IRoleService'
--- dto/user/IUserRequest.ts
+++ dto/user/IUserRequest.ts
@@ -6,4 +6,5 @@ export interface IUserRequest {
   password: string
   photo: string
   status: number
+  role: number
 }
