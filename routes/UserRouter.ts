2eefcd8ec28116452a7f43cdc2b125dd99d72f18
Bryan Portillo <bportillo701@gmail.com>
2023-07-15 18:44:16 -0600

    adding validations to create a regular user in route

--- routes/UserRouter.ts
+++ routes/UserRouter.ts
@@ -1,17 +1,17 @@
-import { type IUserMiddleWare, type IUserController, type IUserRouter, type IAuthenticationMiddleWare } from '../dto'
+import { body } from 'express-validator'
 import { type Router } from 'express'
+import { type IUserMiddleWare, type IUserController, type IUserRouter, type IAuthenticationMiddleWare } from '../dto'
 import { UserController } from '../controllers'
 import { routeFactory } from '../services'
-import { AuthenticationMiddleWare, UserMiddleWare } from '../middlewares'
-import { body } from 'express-validator'
+import { AuthenticationMiddleWare, UserMiddleWare, validateFields } from '../middlewares'
 
 export class UserRouter implements IUserRouter {
   readonly _router: Router
   readonly route = '/users'
 
   constructor(
-    readonly userController: IUserController = new UserController(),
     readonly authenticationMiddleware: IAuthenticationMiddleWare = new AuthenticationMiddleWare(),
+    readonly userController: IUserController = new UserController(),
     readonly userMiddleware: IUserMiddleWare = new UserMiddleWare(),
     readonly Router: () => Router = routeFactory
   ) {
@@ -33,8 +33,13 @@ export class UserRouter implements IUserRouter {
         body('firstName', 'The first name is mandatory').not().isEmpty().isString().isLength({ max: 30 }).trim(),
         body('lastName', 'The last name is mandatory').not().isEmpty().isString().isLength({ max: 30 }).trim(),
         body('password', 'The password is mandatory and must have at least 6 characters').isLength({ min: 6 }).trim(),
-        body('birthDate', 'The birthdate is mandatory').not().isEmpty().isISO8601(),
-        body('photo', 'The photo must be a valid base64').optional().trim().isBase64()
+        body('birthDate', 'The birthdate is mandatory and must be a valid date').not().isEmpty().isISO8601(),
+        body('photo', 'The photo must be a valid base64')
+          .optional()
+          .trim()
+          .isBase64()
+          .custom(this.userMiddleware.isAPhoto),
+        validateFields
       ],
       this.userController.createUser
     )
