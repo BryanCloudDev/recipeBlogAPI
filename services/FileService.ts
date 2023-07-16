9707d5a499260361c3e5619d48d155f37ffc6fdf
Bryan Portillo <bportillo701@gmail.com>
2023-07-15 21:52:09 -0600

    adding base64 validation for file

--- services/FileService.ts
+++ services/FileService.ts
@@ -18,10 +18,22 @@ export class FileService implements IFileService {
     if (photo === undefined) {
       return true
     }
+
+    const isBase64 = this.isBase64(photo)
+
+    if (!isBase64) {
+      return isBase64
+    }
+
     const allowedFileExtensions = ['image/jpeg', 'image/png']
     const metaData = photo.split(';base64,')[0]
     const fileExtension = metaData.split(':')[1]
 
     return allowedFileExtensions.includes(fileExtension)
+  }
+
+  isBase64 = (file: string): boolean => {
+    const regex = /^data:image\/[a-zA-Z]+;base64,([A-Za-z0-9+/=])+$/
+    return regex.test(file)
   }
 }
