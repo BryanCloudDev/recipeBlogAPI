c62e02a5fe8a8b2b633dc2823e891d6b6c5ff3a6
Bryan Portillo <bportillo701@gmail.com>
2023-07-15 18:00:00 -0600

    adding role service class

---
+++ services/RoleService.ts
@@ -0,0 +1,17 @@
+import { type IRoleRepository, type IRoleService } from '../dto'
+import { type Role } from '../models'
+import { LoggerService } from './LoggerService'
+import { RoleRepository } from '../repositories'
+
+export class RoleService implements IRoleService {
+  constructor(private readonly repository: IRoleRepository = new RoleRepository()) {}
+
+  getRoleByIdService = async (id: number): Promise<Role | null> => {
+    try {
+      const role = await this.repository.role.findOne({ where: { id } })
+      return role
+    } catch (error: any) {
+      throw new Error(LoggerService.errorMessageHandler(error, 'Error in get role by id service').message)
+    }
+  }
+}
