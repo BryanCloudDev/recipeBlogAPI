978f8c72313fe876a41a6562c8f41f10fc30dff6
Bryan Portillo <bportillo701@gmail.com>
2023-07-16 23:03:17 -0600

    adding props and metohd to recipe middleware interface

--- dto/recipe/IRecipeMiddleWare.ts
+++ dto/recipe/IRecipeMiddleWare.ts
@@ -1,1 +1,8 @@
-export interface IRecipeMiddleWare {}
+import { type Response, type NextFunction } from 'express'
+import { type ICustomRequest, type IAuthenticationMiddleWare, type IUserMiddleWare } from '..'
+
+export interface IRecipeMiddleWare {
+  validateRecipeId: (req: ICustomRequest, res: Response, next: NextFunction) => Promise<Response | undefined>
+  authenticationMiddleware: IAuthenticationMiddleWare
+  userMiddleware: IUserMiddleWare
+}
