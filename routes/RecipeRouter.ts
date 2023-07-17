e5d8d4c6cff08531bab7e1e33d791c73b78b9db7
Bryan Portillo <bportillo701@gmail.com>
2023-07-16 23:04:01 -0600

    adding custom validations to patch and post

--- routes/RecipeRouter.ts
+++ routes/RecipeRouter.ts
@@ -1,12 +1,24 @@
 import { type IRecipeRouter, type IRecipeController, type IRecipeMiddleWare } from '../dto'
 import { type Router } from 'express'
 import { RecipeController } from '../controllers'
-import { routeFactory } from '../services'
-import { RecipeMiddleWare } from '../middlewares'
+import { Roles, routeFactory } from '../services'
+import { RecipeMiddleWare, validateFields, validateFile } from '../middlewares'
+import { body, checkExact } from 'express-validator'
 
 export class RecipeRouter implements IRecipeRouter {
   public readonly _router: Router
   public readonly route = '/recipes'
+  private readonly recipeValidations = [
+    body('title', 'The title is required').notEmpty().isString().trim(),
+    body('description', 'The description is required').notEmpty().isString().trim(),
+    body('photo', 'The photo must be a valid base64 value').notEmpty().isString().trim(),
+    body('steps', 'Steps must be a valid array').isArray({ min: 1 }),
+    body('steps.*.order', 'The order is required in step object').notEmpty().isInt(),
+    body('steps.*.title', 'The title is required in step object').notEmpty().isString(),
+    body('ingredients', 'Ingredients must be a valid array').isArray({ min: 1 }),
+    body('ingredients.*.order', 'The order is required in ingredient object').notEmpty().isInt(),
+    body('ingredients.*.title', 'The title is required in ingredient object').notEmpty().isString()
+  ]
 
   constructor(
     private readonly recipeController: IRecipeController = new RecipeController(),
@@ -27,7 +39,17 @@ export class RecipeRouter implements IRecipeRouter {
   }
 
   private createRecipe(): void {
-    this._router.post('/', this.recipeController.createRecipe)
+    this._router.post(
+      '/',
+      [
+        this.recipeMiddleWare.authenticationMiddleware.validateJWT,
+        this.recipeMiddleWare.authenticationMiddleware.validateRole([Roles.ADMIN]),
+        checkExact([...this.recipeValidations], { message: 'Too many fields specified' }),
+        validateFields,
+        validateFile
+      ],
+      this.recipeController.createRecipe
+    )
   }
 
   private getAllRecipes(): void {
@@ -47,7 +69,20 @@ export class RecipeRouter implements IRecipeRouter {
   }
 
   private updateRecipeById(): void {
-    this._router.patch('/', this.recipeController.updateRecipeById)
+    this._router.patch(
+      '/:id',
+      [
+        this.recipeMiddleWare.authenticationMiddleware.validateJWT,
+        this.recipeMiddleWare.authenticationMiddleware.validateRole([Roles.ADMIN]),
+        checkExact([...this.recipeValidations.map(validation => validation.optional())], {
+          message: 'Too many fields specified'
+        }),
+        validateFields,
+        validateFile,
+        this.recipeMiddleWare.validateRecipeId
+      ],
+      this.recipeController.updateRecipeById
+    )
   }
 
   public get router(): Router {
