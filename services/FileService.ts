931b040ff6b602b2ea29c881b5628f5043205bf7
Bryan Portillo <bportillo701@gmail.com>
2023-07-30 23:08:50 -0600

    adding dirname to deletexisting file

--- services/FileService.ts
+++ services/FileService.ts
@@ -1,12 +1,12 @@
-import path from 'path'
 import { type IFileService } from '../dto'
 import fs from 'fs/promises'
+import { __dirname } from './utils'
 
 export class FileService implements IFileService {
   deleteExistingFile = async (pathFile: string): Promise<void> => {
     try {
       if (pathFile !== undefined) {
-        await fs.unlink(path.resolve(`/files${pathFile}`))
+        await fs.unlink(`${__dirname}/files/${pathFile}`)
       }
     } catch (error) {
       console.log(error)
