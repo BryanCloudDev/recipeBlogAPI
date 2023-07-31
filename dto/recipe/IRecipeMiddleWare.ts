9541d67b2faebf0483d664731b6d165b16246f41
Bryan Portillo <bportillo701@gmail.com>
2023-07-30 22:14:57 -0600

    adding fileMiddleware to interface

--- dto/recipe/IRecipeMiddleWare.ts
+++ dto/recipe/IRecipeMiddleWare.ts
@@ -1,9 +1,10 @@
 import { type Response, type NextFunction } from 'express'
-import { type ICustomRequest, type IAuthenticationMiddleWare, type IUserMiddleWare } from '..'
+import { type ICustomRequest, type IAuthenticationMiddleWare, type IUserMiddleWare, type IFileMiddleWare } from '..'
 
 export interface IRecipeMiddleWare {
   validateRecipeId: (req: ICustomRequest, res: Response, next: NextFunction) => Promise<Response | undefined>
   validateRecipeOnDelete: (req: ICustomRequest, res: Response, next: NextFunction) => Promise<Response | undefined>
   authenticationMiddleware: IAuthenticationMiddleWare
   userMiddleware: IUserMiddleWare
+  fileMiddleWare: IFileMiddleWare
 }
