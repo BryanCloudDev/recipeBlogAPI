a544aee22a11bedddc26d18dc5a7e1367a54474c
Bryan Portillo <bportillo701@gmail.com>
2023-07-31 23:46:45 -0600

    adding filter param to get recipes method interface

--- dto/recipe/IRecipeService.ts
+++ dto/recipe/IRecipeService.ts
@@ -1,10 +1,10 @@
-import { type IFileService, type IRecipeRequest } from '../'
+import { type IFilter, type IFileService, type IRecipeRequest } from '../'
 import { type User, type Recipe } from '../../models'
 
 export interface IRecipeService {
   createRecipeInstanceService: (recipeRequest: IRecipeRequest) => Recipe
   createRecipeService: (recipe: Recipe, user: User) => Promise<number>
-  getAllRecipesService: () => Promise<Recipe[]>
+  getAllRecipesService: (filter?: IFilter<Recipe>) => Promise<{ recipes: Recipe[]; count: number }>
   getRecipebyIdService: (id: number) => Promise<Recipe | null>
   getRecipesBySearchService: (search: string) => Promise<Recipe[]>
   updateRecipeByIdService: (id: number, recipe: Partial<IRecipeRequest>) => Promise<void>
