9a8d80aed599153fd2193eda16dfc3d89bfa9ae6
Bryan Portillo <bportillo701@gmail.com>
2023-08-01 00:43:03 -0600

    adding get all items function

--- services/utils.ts
+++ services/utils.ts
@@ -1,5 +1,6 @@
 import express, { type Application, Router } from 'express'
 import { dirname } from 'path'
+import { type IGetAllItems, type EntityType, type IGetAllItemsResult } from '../dto'
 
 export const appFactory = (): Application => {
   return express()
@@ -18,3 +19,26 @@ export const __filename = (file = '../index.js'): string => {
 }
 
 export const __dirname = dirname(__filename())
+
+export const getAllItems = async <T extends EntityType>({
+  filter,
+  repository
+}: IGetAllItems<T>): Promise<IGetAllItemsResult<T>> => {
+  const entityPromise = repository.find(
+    filter !== undefined
+      ? {
+          where: filter.where,
+          skip: filter.offset,
+          take: filter.limit,
+          select: filter.select,
+          order: filter.order,
+          relations: filter.relations
+        }
+      : {}
+  )
+  const countPromise = repository.count(filter !== undefined ? { where: filter.where } : {})
+
+  const [items, count] = await Promise.all([entityPromise, countPromise])
+
+  return { items, count }
+}
