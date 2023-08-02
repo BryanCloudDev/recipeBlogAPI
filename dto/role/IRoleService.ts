3807d30e25150731e885bf135883f91c4eee85cd
Bryan Portillo <bportillo701@gmail.com>
2023-08-02 17:55:09 -0600

    creating Role Request Service

--- dto/role/IRoleService.ts
+++ dto/role/IRoleService.ts
@@ -1,5 +1,8 @@
 import { type Role } from '../../models'
+import { type IRoleRequest } from '../IRoleRequest'
 
 export interface IRoleService {
+  createRoleInstanceService: (roleRequest: IRoleRequest) => Role
+  createRoleService: (role: Role) => Promise<Role>
   getRoleByIdService: (id: number) => Promise<Role | null>
 }
